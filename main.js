c202f690523aeeb1fa624d97c493e7d00fd4ca35
Jose Estevez <jose_estevez@outlook.com>
2023-04-06 16:07:26 -0500

    change messages

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import './style.css'
 
 document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
+  <h1>Tienda de Jose!</h1>
+  <h1>Comming Soon</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
